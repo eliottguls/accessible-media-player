1970d7cd8e9d2393c43dd9e96e68658a6a731330
Youen <youenlebihan29@gmail.com>
2026-02-03 14:28:09 +0100

    feat: Film type

---
+++ src/types.tsx
@@ -0,0 +1,12 @@
+export interface FilmInfo {
+  file_url: string;
+  title: string;
+  synopsis_url: string;
+}
+export interface FilmData {
+  film: FilmInfo;
+  subtitles: undefined;
+  audiodescription: undefined;
+  chapters: undefined;
+  poi: undefined;
+}
